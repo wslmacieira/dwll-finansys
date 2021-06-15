65630cf2459bf54abb0537589ff641f84c6471b7
wslmacieira <wslmacieira@outlook.com>
2021-06-15 17:05:12 -0300

    adding EntryFormComponent

--- src/app/pages/categories/categories.module.ts
+++ src/app/pages/categories/categories.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoryListComponent } from './category-list/category-list.component';
 import { CategoryFormComponent } from './category-form/category-form.component';
-import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
