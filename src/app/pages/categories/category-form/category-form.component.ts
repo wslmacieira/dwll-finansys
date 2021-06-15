ca28265791d6ef57d6674504c0453fbfbbba1d4b
wslmacieira <wslmacieira@outlook.com>
2021-06-15 08:50:41 -0300

    adding improving CategoryFormComponent

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -7,7 +7,7 @@ import { CategoryService } from "../shared/category.service";
 
 import { switchMap } from "rxjs/operators";
 
-// import toastr from "toastr";
+import * as toastr from "toastr";
 
 @Component({
   selector: 'app-category-form',
@@ -38,6 +38,15 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
 
   ngAfterContentChecked(): void {
     this.setPageTitle();
+  }
+
+  submitForm() {
+    this.submittingForm = true;
+    if(this.currentAction == 'new') {
+      this.createCategory();
+    } else {
+      this.updateCategory();
+    }
   }
 
   // PRIVATE MÉTHODS
@@ -80,6 +89,45 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
     } else {
       const categoryName = this.category.name || "";
       this.pageTitle = `Editando Categoria: ${categoryName}`
+    }
+  }
+
+  private createCategory() {
+    const category: Category = Object.assign(new Category(), this.categoryForm.value);
+
+    this.categoryService.create(category).subscribe(
+      (category) => this.actionsForSuccess(category),
+      (error) => this.actionsForError(error)
+    )
+  }
+
+  private updateCategory() {
+    const category: Category = Object.assign(new Category(), this.categoryForm.value);
+
+    this.categoryService.update(category).subscribe(
+      (category) => this.actionsForSuccess(category),
+      (error) => this.actionsForError(error)
+    )
+  }
+
+  private actionsForSuccess(category: Category) {
+    toastr.success("Solicitação processada com sucesso!");
+
+    // redirect/reload component page
+    this.router.navigateByUrl("categories", {skipLocationChange: true}).then(
+      () => this.router.navigate(['categories', category.id, 'edit'])
+    )
+  }
+
+  private actionsForError(error: any) {
+    toastr.error("Ocorreu um erro ao processar a solicitação!");
+
+    this.submittingForm = false;
+
+    if(error.status === 422) {
+      this.serverErrorMessages = JSON.parse(error._body).errors;
+    } else {
+      this.serverErrorMessages = ["Falha na comunicação com servidor. Por favor, tente mais tarde."]
     }
   }
 
